56f542351654aa6c5d772b2e58cca042456084e6
Tahmina <tahminakhatun5447@gmail.com>
2022-11-24 22:34:07 +0600

    add advertise collectiions

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run(){
     const productsCollection=client.db("ComfortZone").collection("Products");
     const addCollection=client.db("ComfortZone").collection("Advertise");
     
-   //add product
+   //add product cullections
     app.get('/categories',async(req,res)=>{
       const query={};
       const categories=await categoriesCollection.find(query).toArray();
